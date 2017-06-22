7a0b84b85d15473f05c8f631eeb733ba1daeb791
Mario Smolcic <msmolcic92@gmail.com>
2017-06-22 10:21:09 +0200

    Initial commit.

---
+++ Projection/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projection
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
